16b184c64247bb4a62c1b21a27355981188322bf
georgi-dev <georgideveloper@gmail.com>
2016-07-21 16:17:32 +0300

    Complex-Condition - Project
    All tasks from Complex  - Condition project

--- Complex-Conditions/Invalid Number/Properties/AssemblyInfo.cs
+++ Complex-Conditions/Invalid Number/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hello")]
+[assembly: AssemblyTitle("Invalid Number")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Hello")]
+[assembly: AssemblyProduct("Invalid Number")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f1b92263-96d5-43f3-80d3-bd3ed43af873")]
+[assembly: Guid("8b574590-a128-4060-83cd-af63840e0b42")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Complex-Conditions/Trade Comissions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trade_Comissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine().ToLower();
+            double sales = double.Parse(Console.ReadLine());
+            var comission = -1;
+            var newNum = 33 % 5 ;
+
+            Console.WriteLine(newNum);
+
+
+            /*if (city == "sofia")
+            {///Град	0 ≤ s ≤ 500	500 < s ≤ 1 000	1 000 < s ≤ 10 000	s > 10 000
+                if (sales >= 0 && sales <= 500) comission = 5 / 100;
+                else if (sales > 500 && sales <= 1000) comission = 7 / 100;
+                else if (sales > 1000 && sales <= 10000) comission = 8 / 100;
+                else if (sales > 10000) comission = 12 / 100;
+
+            }*/
+        }
+    }
+}
